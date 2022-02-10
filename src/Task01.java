43ed662323e0a4bb43af3ad235e0de50a4e257f0
ahinski <ahinski.inc@gmail.com>
2022-02-11 00:37:11 +0300

    final Task 0.1

--- src/Task01.java
+++ src/Task01.java
@@ -1,69 +1,90 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-public class Task01 {
-    public static void main(String[] args) throws IOException {
-        LinkedHashSet<Long> set = new LinkedHashSet<>();
-        ArrayList<Long> left = new ArrayList<>();
-        ArrayList<Long> right = new ArrayList<>();
+public class Task01 implements Runnable{
+    static class Node {
+        long key;
+        Node right;
+        Node left;
+
+        Node(long key) {
+            this.key = key;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static class Tree {
+        Node root;
+
+        private Node insertRecursively(Node node, long key) {
+            if (node == null)
+                return new Node(key);
+
+            if (key < node.key) {
+                node.left = insertRecursively(node.left, key);
+            } else if (key > node.key) {
+                node.right = insertRecursively(node.right, key);
+            } else {
+                return node;
+            }
+
+            return node;
+        }
+
+        public void insert(long key) {
+            root = insertRecursively(root, key);
+        }
+
+        public List<Long> preOrderTraversal(Node node, List<Long> list) {
+            if(node != null) {
+                list.add(node.key);
+                preOrderTraversal(node.left, list);
+                preOrderTraversal(node.right, list);
+            }
+            return list;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(null, new Task01(), "", 256 * 1024 * 1024).start();
+    }
+
+    public void run() {
+        Tree tree = new Tree();
         File file = new File("inputs//input01.txt");
-        Scanner sc = new Scanner(file);
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         while(sc.hasNext())
-            set.add(sc.nextLong());
+            tree.insert(sc.nextLong());
         sc.close();
-        Iterator<Long> iterator = set.iterator();
-        long root = (Long)iterator.next();
-        for(Long key : set) {
-            if(key <= root)
-                left.add(key);
-            else right.add(key);
+        List<Long> list = new LinkedList<>();
+        list = tree.preOrderTraversal(tree.root, list);
+        file = new File("outputs//output01.txt");
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(file);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        FileWriter fw = new FileWriter("outputs//output01.txt");
-        Long father = left.get(0);
-        left.remove(0);
-        fw.write(father.toString() + "\n");
-        boolean exists = false;
-        while(left.size() != 0) {
-            for(Long key : left) {
-                if(key < father) {
-                    exists = true;
-                    father = key;
-                    left.remove(key);
-                    fw.write(father + "\n");
-                    break;
-                }
-            }
-            if(!exists) {
-                father = left.get(0);
-                left.remove(0);
-                fw.write(father.toString() + "\n");
-            }
-            exists = false;
-        }
-        if(right.size() != 0) {
-            father = right.get(0);
-            right.remove(0);
-            fw.write(father.toString() + "\n");
-            while (right.size() != 0) {
-                for (Long key : right) {
-                    if (key > father) {
-                        exists = true;
-                        father = key;
-                        right.remove(key);
-                        fw.write(father + "\n");
-                        break;
-                    }
-                }
-                if (!exists) {
-                    father = right.get(0);
-                    right.remove(0);
-                    fw.write(father.toString() + "\n");
-                }
-                exists = false;
+        for(Long key : list) {
+            try {
+                fw.write(key.toString() + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        fw.close();
+        try {
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 } 
