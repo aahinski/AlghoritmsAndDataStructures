f7028d63f37a9102878051af31c364e9e517f56c
ahinski <ahinski.inc@gmail.com>
2022-02-13 17:27:40 +0300

    Task 0.3 right solution, but not fast enough

--- src/Trees/Task03.java
+++ src/Trees/Task03.java
@@ -4,64 +4,67 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Task03 implements Runnable {
     static class Node {
         int key;
-        Node left;
-        Node right;
+        Node parent;
+        boolean left;
 
         Node(int key) {
             this.key = key;
-            this.left = this.right = null;
+            this.parent = null;
+            this.left = true;
         }
+
+        Node(Node node) {
+            this.key = node.key;
+            this.parent = node.parent;
+            this.left = node.left;
+        }
+
+        Node() {}
     }
 
-    Node[] binaryTreeArray(String inputFileName) throws FileNotFoundException {
-        File file = new File(inputFileName);
+    boolean isBinarySearchedTree(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
         Scanner sc = new Scanner(file);
         Node[] tree = new Node[Integer.parseInt(sc.nextLine())];
         tree[0] = new Node(Integer.parseInt(sc.nextLine()));
         int i = 1;
         while(sc.hasNext()) {
+            Node node = new Node();
             String[] fields = sc.nextLine().split(" ");
-            Node node = new Node(Integer.parseInt(fields[0]));
-            tree[i] = node;
+            node.key = Integer.parseInt(fields[0]);
+            node.parent = tree[Integer.parseInt(fields[1]) - 1];
             if(fields[2].equals("L"))
-                tree[Integer.parseInt(fields[1]) - 1].left = node;
-            else tree[Integer.parseInt(fields[1]) - 1].right = node;
+                node.left = true;
+            else node.left = false;
+            Node tmp = new Node(node);
+            while(tmp != tree[0]) {
+                boolean left = tmp.left;
+                if(left) {
+                    if (node.key >= tmp.parent.key)
+                        return false;
+                } else {
+                    if (node.key < tmp.parent.key)
+                        return false;
+                }
+                tmp = tmp.parent;
+            }
+            tree[i] = node;
             i++;
         }
-        sc.close();
-        return tree;
-    }
-
-    boolean isBinarySearchedTree(Node root) {
-        if(root == null)
-            return true;
-
-        if(root.left != null)
-            if(root.key <= root.left.key)
-                return false;
-
-        if(root.right != null)
-            if(root.key > root.right.key)
-                return false;
-
-        return isBinarySearchedTree(root.left) && isBinarySearchedTree(root.right);
+        return true;
     }
 
     public static void main(String[] args) { new Thread(null, new Task03(), "", 64 * 1024 * 1024).start() ; }
 
     public void run() {
-        Node[] tree = new Node[0];
-        try {
-            tree = binaryTreeArray("inputs//input03.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        boolean isBinarySearchedTree = isBinarySearchedTree(tree[0]);
+        LocalDateTime from = LocalDateTime.now();
         File file = new File("outputs//output03.txt");
         FileWriter fw = null;
         try {
@@ -69,24 +72,19 @@ public class Task03 implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(isBinarySearchedTree == true) {
-            try {
+        try {
+            if(isBinarySearchedTree("inputs//input03.txt"))
                 fw.write("YES");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            try {
-                fw.write("NO");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            else fw.write("NO");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         try {
             fw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        LocalDateTime to = LocalDateTime.now();
+        System.out.println(Duration.between(from, to).toMillis());
     }
 }
