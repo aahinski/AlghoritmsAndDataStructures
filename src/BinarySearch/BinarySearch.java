bd70c42d12364a6026eb30ad174b83a4ead33714
ahinski <ahinski.inc@gmail.com>
2022-02-23 01:01:35 +0300

    Binary Search Implementation Non Recursively

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,71 +1,86 @@
 package BinarySearch;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class BinarySearch implements Runnable {
+public class BinarySearch {
 
-    public static boolean binarySearch(int left, int right, int[] array, int x) {
-        int medium = left + ((right - left) / 2);
+    static int findFirstElementGreaterThanX(int array[], int x, int i, int length) {
+        int left = i;
+        int right = length - 1;
+        int medium;
 
-        if(left > right) {
-            System.out.print(0);
-            System.out.print(" ");
-            if(medium > array.length - 1 || medium < 0) {
-                System.out.print(array.length);
-                System.out.print(" ");
-                System.out.print(array.length);
-            }
-            else if(array[medium] > x) {
-                System.out.print(medium);
-                System.out.print(" ");
-                System.out.print(medium);
+        while (left <= right) {
+            medium = left + (right - left) / 2;
+
+            if (medium != length - 1) {
+                if (array[medium] > x && array[medium - 1] == x)
+                    return medium;
             } else {
-                int i = medium - 1;
-                System.out.print(i);
-                System.out.print(" ");
-                System.out.print(i);
+                if (array[medium] > x)
+                    return length - 1;
             }
-            System.out.print(" " + "\n");
-            return false;
+
+            if (array[medium] <= x) left = medium + 1;
+            else right = medium - 1;
         }
 
-        if(array[medium] > x)
-            binarySearch(left, medium - 1, array, x);
-        else if(array[medium] < x)
-            binarySearch(medium + 1, right, array, x);
-        else if(array[medium] == x) {
-            System.out.print(1);
-            System.out.print(" ");
-            int i = medium;
-            while(i > -1 && array[i] == x)
-                i--;
-            System.out.print(i + 1);
-            System.out.print(" ");
-            i = medium;
-            while(i < array.length && array[i] == x)
-                i++;
-            System.out.print(i);
-            System.out.print(" " + "\n");
+        return length;
+    }
+
+    public static void binarySearch(int array[], int x, int length)
+    {
+        int left = 0;
+        int right = length - 1;
+        int medium;
+        int i;
+        int j;
+
+        while (left <= right) {
+            medium = left + (right - left) / 2;
+
+            if(medium != 0) {
+                if (array[medium] == x && array[medium - 1] != x) {
+                    i = medium;
+                    System.out.print(1 + " " + i + " " + findFirstElementGreaterThanX(array, x, i, length) + "\n");
+                    return;
+                }
+            } else {
+                if(array[0] == x) {
+                    i = 0;
+                    System.out.print(1 + " " + i + " " + findFirstElementGreaterThanX(array, x, i, length) + "\n");
+                    return;
+                }
+            }
+
+            if(array[medium] >= x) right = medium - 1;
+            else left = medium + 1;
         }
-        return true;
+
+        medium = left + (right - left) / 2;
+
+        if(medium > length - 1 || medium < 0)
+            System.out.print(0 + " " + length + " " + length + "\n");
+        else if(array[medium] > x)
+            System.out.print(0 + " " + medium + " " + medium + "\n");
+        else
+            System.out.print(0 + " " + (medium - 1) + " " + (medium - 1) + "\n");
     }
 
-    public static void main(String[] args) {
-        new Thread(null, new BinarySearch(), "", 256 * 1024 * 1024).start();
-    }
-
-    public void run() {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[] array = new int[size];
-        for (int i = 0; i < size; i++)
-            array[i] = sc.nextInt();
-        int requestsQuantity = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+        int[] array = new int[length];
+        String[] strArray = br.readLine().split(" ");
+        for (int i = 0; i < length; i++)
+            array[i] = Integer.parseInt(strArray[i]);
+        int requestsQuantity = Integer.parseInt(br.readLine());
+        String[] strRequests = br.readLine().split(" ");
         int[] requests = new int[requestsQuantity];
         for (int i = 0; i < requestsQuantity; i++)
-            requests[i] = sc.nextInt();
-        sc.close();
+            requests[i] = Integer.parseInt(strRequests[i]);
         for(int request : requests)
-            binarySearch(0, size - 1, array, request);
+            binarySearch(array, request, length);
     }
 }
